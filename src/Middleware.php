6100433184908d8e361c69224e20c6ec00a7fa4e
FizeChan <chenfengzhan@qq.com>
2020-01-29 03:27:41 +0800

    添加范式。

--- src/Middleware.php
+++ src/Middleware.php
@@ -3,8 +3,27 @@
 
 namespace fize\http;
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
-class Middleware
+/**
+ * 请求中间件
+ */
+class Middleware implements MiddlewareInterface
 {
 
+    /**
+     * 参与处理服务器的请求与响应
+     *
+     * 本例只是个范式，无业务逻辑
+     * @param ServerRequestInterface $request 请求
+     * @param RequestHandlerInterface $handler 处理器
+     * @return ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        return new Response();
+    }
 }
--- src/RequestHandler.php
+++ src/RequestHandler.php
@@ -3,8 +3,25 @@
 
 namespace fize\http;
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
-class RequestHandler
+/**
+ * 请求处理器
+ */
+class RequestHandler implements RequestHandlerInterface
 {
 
+    /**
+     * 处理服务器请求并返回响应
+     *
+     * 本例只是个范式，无业务逻辑
+     * @param ServerRequestInterface $request 服务端请求
+     * @return ResponseInterface
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        return new Response();
+    }
 }
