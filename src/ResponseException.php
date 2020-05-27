7c5e73c440623499a3194c0d392aac1c0056088b
FizeChan <chenfengzhan@qq.com>
2020-05-27 10:58:35 +0800

    添加响应异常类。

---
+++ src/ResponseException.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace fize\http;
+
+use RuntimeException;
+
+/**
+ * 响应失败异常
+ */
+class ResponseException extends RuntimeException
+{
+    /**
+     * @var Response 响应对象
+     */
+    protected $response;
+
+    /**
+     * 初始化时设置响应对象
+     * @param Response $response 响应对象
+     */
+    public function __construct(Response $response)
+    {
+        $this->response = $response;
+        parent::__construct($response->getReasonPhrase(), $response->getStatusCode());
+    }
+
+    /**
+     * 获取响应对象
+     * @return Response
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+}
