8abb9329c58462b31dc0a72e674c89b6fe5a656a
FizeChan <chenfengzhan@qq.com>
2021-07-19 17:41:22 +0800

    RequestHandler修改及单元测试。

--- src/RequestHandler.php
+++ src/RequestHandler.php
@@ -15,7 +15,7 @@ class RequestHandler implements RequestHandlerInterface
     /**
      * 处理服务器请求并返回响应
      *
-     * 本例只是个范式，无业务逻辑
+     * 本例只是个范式，无实际业务逻辑
      * @param ServerRequestInterface $request 服务端请求
      * @return ResponseInterface
      */
--- tests/TestRequestHandler.php
+++ tests/TestRequestHandler.php
@@ -2,14 +2,36 @@
 
 
 use fize\http\RequestHandler;
+use fize\http\Response;
+use fize\http\ServerRequest;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 class TestRequestHandler extends TestCase
 {
-    //该类无需进行单元测试
 
     public function testHandle()
     {
+        $sreq = new ServerRequest('GET', 'https://www.baidu.com/');
+        $myrh = new MyRequestHandler();
+        $response = $myrh->handle($sreq);
+        self::assertEquals(404, $response->getStatusCode());
+    }
+}
 
+class MyRequestHandler extends RequestHandler
+{
+    /**
+     * 处理服务器请求并返回响应
+     *
+     * 本例只是个范式，无业务逻辑
+     * @param ServerRequestInterface $request 服务端请求
+     * @return ResponseInterface
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $response = new Response();
+        return $response->withStatus(404);
     }
 }
