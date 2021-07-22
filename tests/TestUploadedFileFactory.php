2bf4d550778bfb9b0fdff9b263e5163c04e8a247
FizeChan <chenfengzhan@qq.com>
2021-07-22 18:05:27 +0800

    UploadedFileFactory修改及单元测试。

--- tests/TestUploadedFileFactory.php
+++ tests/TestUploadedFileFactory.php
@@ -1,17 +1,22 @@
 <?php
 
-
+use fize\http\StreamFactory;
 use fize\http\UploadedFileFactory;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\UploadedFileInterface;
 
-/**
- * @todo 待测试
- */
 class TestUploadedFileFactory extends TestCase
 {
 
     public function testCreateUploadedFile()
     {
-
+        $sfactory = new StreamFactory();
+        $st = $sfactory->createStreamFromFile(__FILE__);
+        $factory = new UploadedFileFactory();
+        $clientFilename = basename(__FILE__);
+        $clientMediaType = 'text/html';
+        $upf = $factory->createUploadedFile($st, $st->getSize(), UPLOAD_ERR_OK, $clientFilename, $clientMediaType);
+        var_dump($upf);
+        self::assertInstanceOf(UploadedFileInterface::class, $upf);
     }
 }
