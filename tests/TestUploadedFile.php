9f5412acc39e3d826a7eb8d2460650eaf8cef113
FizeChan <chenfengzhan@qq.com>
2021-07-22 17:59:10 +0800

    UploadedFile修改及单元测试。

--- tests/TestUploadedFile.php
+++ tests/TestUploadedFile.php
@@ -3,50 +3,79 @@
 
 use fize\http\UploadedFile;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\StreamInterface;
 
-/**
- * @todo 待测试
- */
 class TestUploadedFile extends TestCase
 {
 
-    public function testIsMoved()
-    {
-
-    }
-
-    public function testGetSize()
-    {
-
-    }
-
     public function test__construct()
     {
-
+        $upf = new UploadedFile(__FILE__, filesize(__FILE__), UPLOAD_ERR_OK);
+        var_dump($upf);
+        self::assertInstanceOf(UploadedFile::class, $upf);
     }
 
-    public function testGetError()
+    public function testIsMoved()
     {
-
-    }
-
-    public function testGetClientFilename()
-    {
-
-    }
-
-    public function testGetClientMediaType()
-    {
-
+        $file = dirname(__DIR__) . '/temp/test.txt';
+        $upf = new UploadedFile($file, filesize($file), UPLOAD_ERR_OK);
+        $moved = $upf->isMoved();
+        self::assertFalse($moved);
+        $targetPath = dirname(__DIR__) . '/temp/uploaded.txt';
+        $upf->moveTo($targetPath);
+        $moved = $upf->isMoved();
+        self::assertTrue($moved);
     }
 
     public function testGetStream()
     {
-
+        $upf = new UploadedFile(__FILE__, filesize(__FILE__), UPLOAD_ERR_OK);
+        $stream = $upf->getStream();
+        var_dump($stream);
+        self::assertInstanceOf(StreamInterface::class, $stream);
     }
 
     public function testMoveTo()
     {
+        $file = dirname(__DIR__) . '/temp/test.txt';
+        $upf = new UploadedFile($file, filesize($file), UPLOAD_ERR_OK);
+        $moved = $upf->isMoved();
+        self::assertFalse($moved);
+        $targetPath = dirname(__DIR__) . '/temp/uploaded.txt';
+        $upf->moveTo($targetPath);
+        $moved = $upf->isMoved();
+        self::assertTrue($moved);
+    }
 
+    public function testGetSize()
+    {
+        $upf = new UploadedFile(__FILE__, filesize(__FILE__), UPLOAD_ERR_OK);
+        $size = $upf->getSize();
+        var_dump($size);
+        self::assertIsInt($size);
+    }
+
+    public function testGetError()
+    {
+        $upf = new UploadedFile(__FILE__, filesize(__FILE__), UPLOAD_ERR_EXTENSION);
+        $error = $upf->getError();
+        var_dump($error);
+        self::assertEquals(UPLOAD_ERR_EXTENSION, $error);
+    }
+
+    public function testGetClientFilename()
+    {
+        $upf = new UploadedFile(__FILE__, filesize(__FILE__), UPLOAD_ERR_EXTENSION);
+        $fname = $upf->getClientFilename();
+        var_dump($fname);
+        self::assertEquals(basename(__FILE__), $fname);
+    }
+
+    public function testGetClientMediaType()
+    {
+        $upf = new UploadedFile(__FILE__, filesize(__FILE__), UPLOAD_ERR_EXTENSION);
+        $mime = $upf->getClientMediaType();
+        var_dump($mime);
+        self::assertEquals('text/html', $mime);
     }
 }
