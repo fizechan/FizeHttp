d083d97ca3526df3dfc0dd17c8ffb7a2015c7fc1
FizeChan <chenfengzhan@qq.com>
2021-09-01 09:39:26 +0800

    优化ResponseException，支持传递更多参数。

--- src/ResponseException.php
+++ src/ResponseException.php
@@ -3,6 +3,7 @@
 namespace fize\http;
 
 use RuntimeException;
+use Throwable;
 
 /**
  * 响应失败异常
@@ -16,12 +17,15 @@ class ResponseException extends RuntimeException
 
     /**
      * 初始化时设置响应对象
-     * @param Response $response 响应对象
+     * @param Response       $response 响应对象
+     * @param Throwable|null $previous 前置错误
+     * @param string         $message  错误信息
+     * @param int            $code     错误码
      */
-    public function __construct(Response $response)
+    public function __construct(Response $response, Throwable $previous = null, string $message = "", int $code = 0)
     {
         $this->response = $response;
-        parent::__construct($response->getReasonPhrase(), $response->getStatusCode());
+        parent::__construct($message, $code, $previous);
     }
 
     /**
